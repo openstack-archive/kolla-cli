37260ccf3faa5ab93d98a75b2176da3e52995652
Steve Noyes <steve.noyes@oracle.com>
2015-07-30 12:03:38 -0400

    For Host Install, change the default behavior to prompt the password for the host's root password instead of supplying it on the command line. A switch --insecure is provided to optionally provide the password on the command line. That switch will not be documented.

--- kollaclient/host.py
+++ kollaclient/host.py
@@ -12,6 +12,11 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 import logging
+import getpass
+import argparse
+import traceback
+
+from paramiko import AuthenticationException
 
 from kollaclient.i18n import _
 from kollaclient.sshutils import ssh_check_host
@@ -23,48 +28,54 @@ from kollaclient.utils import save_etc_yaml
 
 from cliff.command import Command
 
+HOSTS_YML_FNAME = 'hosts.yml'
+
+
+def host_not_found(log, hostname):
+    log.info('Host (%s) not found. ' % hostname +
+             'Please add it with "host add hostname"')
+
 
 class HostAdd(Command):
-    "Host Add"
+    """Add host to open-stack-kolla"""
 
     log = logging.getLogger(__name__)
 
     def get_parser(self, prog_name):
         parser = super(HostAdd, self).get_parser(prog_name)
-        parser.add_argument('hostname')
-        parser.add_argument('networkaddress')
-        # TODO(bmace) error if args missing
+        parser.add_argument('hostname', metavar='<hostname>', help='hostname')
+        parser.add_argument('networkaddress', metavar='networkaddress',
+                            help='Network address')
         return parser
 
     def take_action(self, parsed_args):
         hostname = parsed_args.hostname.rstrip()
         netAddr = parsed_args.networkaddress.rstrip()
-        contents = load_etc_yaml('hosts.yml')
+        contents = load_etc_yaml(HOSTS_YML_FNAME)
         for host in contents:
             if host == hostname:
-                # TODO(bmace) fix message
-                self.log.info(_('host already exists (%s)') % hostname)
+                self.log.info(_('Skipping, host (%s) already added.'
+                                % hostname))
                 return
         hostEntry = {hostname: {'Services': '', 'NetworkAddress':
                      netAddr, 'Zone': ''}}
         contents.update(hostEntry)
-        save_etc_yaml('hosts.yml', contents)
+        save_etc_yaml(HOSTS_YML_FNAME, contents)
 
 
 class HostRemove(Command):
-    "Host Remove"
+    """Remove host from openstack-kolla"""
 
     log = logging.getLogger(__name__)
 
     def get_parser(self, prog_name):
         parser = super(HostRemove, self).get_parser(prog_name)
-        parser.add_argument('hostname')
-        # TODO(bmace) error if arg missing
+        parser.add_argument('hostname', metavar='<hostname>', help='hostname')
         return parser
 
     def take_action(self, parsed_args):
         hostname = parsed_args.hostname.rstrip()
-        contents = load_etc_yaml('hosts.yml')
+        contents = load_etc_yaml(HOSTS_YML_FNAME)
         foundHost = False
         for host, _ in contents.items():
             if host == hostname:
@@ -72,19 +83,17 @@ class HostRemove(Command):
         if foundHost:
             del contents[hostname]
         else:
-            # TODO(bmace) fix message
-            self.log.info("no host by name (" + hostname + ") found")
-        save_etc_yaml('hosts.yml', contents)
+            self.log.info('Host (%s) not found. Skipping remove' % hostname)
+        save_etc_yaml(HOSTS_YML_FNAME, contents)
 
 
 class HostList(Command):
-    "Host List"
+    """List all hosts"""
 
     log = logging.getLogger(__name__)
 
     def take_action(self, parsed_args):
-        self.log.info(_("host list"))
-        contents = load_etc_yaml('hosts.yml')
+        contents = load_etc_yaml(HOSTS_YML_FNAME)
         # TODO(bmace) fix output format
         for host, hostdata in contents.items():
             self.log.info(host)
@@ -92,44 +101,43 @@ class HostList(Command):
 
 
 class HostSetzone(Command):
-    "Host Setzone"
+    """Add a host to a zone"""
 
     log = logging.getLogger(__name__)
 
     def take_action(self, parsed_args):
-        self.log.info(_("host setzone"))
+        self.log.info(_('host setzone'))
         self.app.stdout.write(parsed_args)
 
 
 class HostAddservice(Command):
-    "Host Addservice"
+    """add service to a host"""
 
     log = logging.getLogger(__name__)
 
     def take_action(self, parsed_args):
-        self.log.info(_("host addservice"))
+        self.log.info(_('host addservice'))
         self.app.stdout.write(parsed_args)
 
 
 class HostRemoveservice(Command):
-    "Host Removeservice"
+    """Remove service from a host"""
 
     log = logging.getLogger(__name__)
 
     def take_action(self, parsed_args):
-        self.log.info(_("host removeservice"))
+        self.log.info(_('host removeservice'))
         self.app.stdout.write(parsed_args)
 
 
 class HostCheck(Command):
-    "Host Check"
+    """Check if openstack-kolla is installed"""
 
     log = logging.getLogger(__name__)
 
     def get_parser(self, prog_name):
         parser = super(HostCheck, self).get_parser(prog_name)
-        parser.add_argument('hostname')
-        # TODO(bmace) error if arg missing
+        parser.add_argument('hostname', metavar='<hostname>', help='hostname')
         return parser
 
     def take_action(self, parsed_args):
@@ -138,45 +146,43 @@ class HostCheck(Command):
             try:
                 ssh_keygen()
             except Exception as e:
-                self.log.error("Error generating ssh keys: %s" % str(e))
+                self.log.error_(('Error generating ssh keys: %s' % str(e)))
                 return False
 
         hostname = parsed_args.hostname.rstrip()
         netAddr = None
-        contents = load_etc_yaml('hosts.yml')
+        contents = load_etc_yaml(HOSTS_YML_FNAME)
         hostFound = False
         for host, hostdata in contents.items():
             if host == hostname:
-                # TODO(bmace) fix message
                 hostFound = True
                 netAddr = hostdata['NetworkAddress']
                 break
 
-        if hostFound is False:
-            self.log.error("No host by name (%s) found" % hostname)
+        if not hostFound:
+            host_not_found(self.log, hostname)
             return False
 
         try:
-            self.log.info('host check (%s) at address (%s)' %
+            self.log.info('Starting host (%s) check at address (%s)' %
                           (hostname, netAddr))
             ssh_check_host(netAddr)
-            self.log.info('host check (%s) passed' % hostname)
+            self.log.info('Host (%s), check succeeded' % hostname)
             return True
         except Exception as e:
-            self.log.error("host check (%s) failed (%s)" % (hostname, str(e)))
+            self.log.error('Host (%s), check failed (%s)' % (hostname, str(e)))
             return False
 
 
 class HostInstall(Command):
-    "Host Install"
+    """Install openstack-kolla on host"""
 
     log = logging.getLogger(__name__)
 
     def get_parser(self, prog_name):
         parser = super(HostInstall, self).get_parser(prog_name)
-        parser.add_argument('hostname')
-        parser.add_argument('password')
-        # TODO(bmace) error if arg missing
+        parser.add_argument('hostname', metavar='<hostname>', help='hostname')
+        parser.add_argument('--insecure', nargs='?', help=argparse.SUPPRESS)
         return parser
 
     def take_action(self, parsed_args):
@@ -185,42 +191,51 @@ class HostInstall(Command):
             try:
                 ssh_keygen()
             except Exception as e:
-                self.log.error("Error generating ssh keys: %s" % str(e))
+                self.log.error('Error generating ssh keys: %s' % str(e))
                 return False
 
-        hostname = parsed_args.hostname.rstrip()
-        # TODO(bmace) get this password in another way?
-        password = parsed_args.password.rstrip()
+        hostname = parsed_args.hostname.strip()
+
         netAddr = None
-        contents = load_etc_yaml('hosts.yml')
+        contents = load_etc_yaml(HOSTS_YML_FNAME)
         hostFound = False
         for host, hostdata in contents.items():
             if host == hostname:
-                # TODO(bmace) fix message
                 hostFound = True
                 netAddr = hostdata['NetworkAddress']
                 break
 
-        if hostFound is False:
-            self.log.info("no host by name (%s) found" % hostname)
+        if not hostFound:
+            host_not_found(self.log, hostname)
             return False
 
-        # Don't bother doing all the install stuff is the check looks ok
-        sshCheck = False
+        # Don't bother doing all the install stuff if the check looks ok
         try:
             ssh_check_host(netAddr)
-            self.log.info('host install skipped for (%s) as check passed' % hostname)
+            self.log.info('Install skipped for host (%s), ' % hostname +
+                          'kolla already installed')
             return True
-        except Exception as e:
+
+        except AuthenticationException as e:
+            # ssh check failed
             pass
 
-        # If sshCheck fails we need to set up the user / remote ssh keys
+        except Exception as e:
+            self.log.error('Unexpected exception: %s' % traceback.format_exc())
+            raise e
+
+        # sshCheck failed- we need to set up the user / remote ssh keys
         # using root and the available password
-        if sshCheck is False:
-            try: 
-                self.log.info('host install (%s) at address (%s)' %
-                              (hostname, netAddr))
-                ssh_install_host(netAddr, password)
-                self.log.info('host install (%s) passed' % hostname)
-            except Exception as e:
-                self.log.info('host install (%s) failed (%s)' % (hostname, str(e)))
+        if parsed_args.insecure:
+            password = parsed_args.insecure.strip()
+        else:
+            password = getpass.getpass('Root password of %s: ' % hostname)
+
+        try:
+            self.log.info('Starting install of host (%s) at %s)' %
+                          (hostname, netAddr))
+            ssh_install_host(netAddr, password)
+            self.log.info('Host (%s) install succeeded' % hostname)
+        except Exception as e:
+            self.log.info('Host (%s) install failed (%s)'
+                          % (hostname, str(e)))
