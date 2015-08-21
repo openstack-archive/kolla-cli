a3fb683f18d59a2b17c0ad3ec3a31e03b8482d9c
Borne Mace <borne.mace@oracle.com>
2015-08-21 15:37:43 -0700

    removed unneeded service commands.  removed reference to removed defaults.yml config file.  lowered pbr version requirement.

--- kollacli/service.py
+++ kollacli/service.py
@@ -16,40 +16,8 @@ import traceback
 
 from kollacli.ansible.inventory import Inventory
 from kollacli.exceptions import CommandError
-from kollacli.i18n import _
 
-from cliff.command import Command
 from cliff.lister import Lister
-
-
-class ServiceActivate(Command):
-    "Service Activate"
-
-    log = logging.getLogger(__name__)
-
-    def take_action(self, parsed_args):
-        self.log.info(_("service activate"))
-        self.app.stdout.write(parsed_args)
-
-
-class ServiceDeactivate(Command):
-    "Service Deactivate"
-
-    log = logging.getLogger(__name__)
-
-    def take_action(self, parsed_args):
-        self.log.info(_("service deactivate"))
-        self.app.stdout.write(parsed_args)
-
-
-class ServiceAutodeploy(Command):
-    "Service Autodeploy"
-
-    log = logging.getLogger(__name__)
-
-    def take_action(self, parsed_args):
-        self.log.info(_("service autodeploy"))
-        self.app.stdout.write(parsed_args)
 
 
 class ServiceList(Lister):
