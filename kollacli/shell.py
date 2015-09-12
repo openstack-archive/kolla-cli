37877e6aa8794ea799524cae39b6b82f77891779
Borne Mace <borne.mace@oracle.com>
2015-09-11 21:14:09 -0700

    kolla is under openstack now rather than stackforge, fixed string

--- kollacli/shell.py
+++ kollacli/shell.py
@@ -23,7 +23,7 @@ class KollaCli(App):
 
     def __init__(self):
         super(KollaCli, self).__init__(
-            description='Command-Line Client for StackForge Kolla',
+            description='Command-Line Client for OpenStack Kolla',
             version='0.1',
             command_manager=CommandManager('kolla.cli'),
             )
