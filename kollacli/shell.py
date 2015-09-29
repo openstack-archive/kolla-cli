2002ec213c5203009b7e929da294f07b0bb56312
Borne Mace <borne.mace@oracle.com>
2015-09-28 17:06:01 -0700

    changed permission check to verify against inventory file access rather than group membership, since root shouldn't need to be in the kolla group.

--- kollacli/shell.py
+++ kollacli/shell.py
@@ -21,9 +21,10 @@ import sys
 from cliff.app import App
 from cliff.commandmanager import CommandManager
 
+from kollacli.ansible.inventory import INVENTORY_PATH
 from kollacli.exceptions import CommandError
-from kollacli.utils import get_admin_user
 from kollacli.utils import get_kolla_log_dir
+from kollacli.utils import get_kollacli_etc
 
 
 class KollaCli(App):
@@ -37,14 +38,17 @@ class KollaCli(App):
             )
 
         # check that current user is in the kolla group
-        user = getpass.getuser()
-        group = get_admin_user()
-        group_info = grp.getgrnam(group)
-        if user not in group_info.gr_mem:
-            raise CommandError('User (%s) must be a member ' % user +
-                               'of the %s group. ' % group +
-                               '\nPlease add user to group and ' +
-                               'then log out and back in.')
+        inventory_path = os.path.join(get_kollacli_etc(),
+                                      INVENTORY_PATH)
+        inventory_file = None
+        try:
+            inventory_file = open(inventory_path, 'r+')
+        except Exception:
+            raise CommandError('permission denied to run kollacli' +
+                               ', add this user to the kolla group')
+        finally:
+            if inventory_file and inventory_file.close is False:
+                inventory_file.close()
 
         self.rotating_log_dir = get_kolla_log_dir()
         self.max_bytes = 500000
