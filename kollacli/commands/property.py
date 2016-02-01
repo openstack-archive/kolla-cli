c8122d120a171fabb72a6d1786543f63252291d3
Steve Noyes <steve.noyes@oracle.com>
2016-02-01 14:01:20 -0500

    fix argparse when groups and hosts not specified
    - if --groups or --hosts are specified without any groups
    or hosts, then the global property will unexpectedly be set.
    This changes the parsing so that if those switches are present,
    hosts or groups must be specified.
    
    - make arg parse meta_vars & help consistent for set/clear/list
    
    - make loading of properties more efficient. Only load properties
    from files if they are actually going to be used.
    
    Jira-Issue: OPENSTACK-560

--- kollacli/commands/property.py
+++ kollacli/commands/property.py
@@ -23,6 +23,14 @@ from cliff.command import Command
 from cliff.lister import Lister
 
 
+def _get_names(args_list):
+    csv_list = args_list[0].strip()
+    names = utils.convert_to_unicode(csv_list).split(',')
+    if 'all' in names:
+        names = None
+    return names
+
+
 class PropertySet(Command):
     "Property Set"
 
@@ -32,50 +40,50 @@ class PropertySet(Command):
                             help=u._('Property name'))
         parser.add_argument('propertyvalue', metavar='<propertyvalue',
                             help=u._('Property value'))
-        parser.add_argument('--hosts', nargs='?',
-                            metavar='<host_name>',
-                            help=u._('Property host'))
-        parser.add_argument('--groups', nargs='?',
-                            metavar='<group_name>',
-                            help=u._('Property group'))
+        parser.add_argument('--hosts', nargs=1,
+                            metavar='<host_list>',
+                            help=u._('Property host list'))
+        parser.add_argument('--groups', nargs=1,
+                            metavar='<group_list>',
+                            help=u._('Property group list'))
         return parser
 
     def take_action(self, parsed_args):
         try:
             property_name = parsed_args.propertyname.strip()
             property_value = parsed_args.propertyvalue.strip()
-            ansible_properties = properties.AnsibleProperties()
-            hosts = None
-            groups = None
 
             if parsed_args.hosts:
-                host_list = parsed_args.hosts.strip()
-                hosts = utils.convert_to_unicode(host_list).split(',')
+                if parsed_args.groups:
+                    raise CommandError(
+                        u._('Invalid to use both hosts and groups arguments ' +
+                            'together.'))
 
-            if parsed_args.groups:
-                group_list = parsed_args.groups.strip()
-                groups = utils.convert_to_unicode(group_list).split(',')
-
-            if hosts is None and groups is None:
-                ansible_properties.set_property(property_name, property_value)
-            elif hosts is not None and groups is not None:
-                raise CommandError(
-                    u._('Invalid to use both hosts and groups arguments '
-                        'together.'))
-            elif hosts is not None:
-                for host_name in hosts:
-                    if host_name == 'all':
-                        hosts = None
-                        break
+                host_names = _get_names(parsed_args.hosts)
+                ansible_properties = \
+                    properties.AnsibleProperties(load_globals=False,
+                                                 load_groups=False,
+                                                 load_hosts=True)
                 ansible_properties.set_host_property(property_name,
-                                                     property_value, hosts)
-            elif groups is not None:
-                for group_name in groups:
-                    if group_name == 'all':
-                        groups = None
-                        break
+                                                     property_value,
+                                                     host_names)
+
+            elif parsed_args.groups:
+                group_names = _get_names(parsed_args.groups)
+                ansible_properties = \
+                    properties.AnsibleProperties(load_globals=False,
+                                                 load_groups=True,
+                                                 load_hosts=False)
                 ansible_properties.set_group_property(property_name,
-                                                      property_value, groups)
+                                                      property_value,
+                                                      group_names)
+            else:
+                ansible_properties = \
+                    properties.AnsibleProperties(load_globals=True,
+                                                 load_groups=False,
+                                                 load_hosts=False)
+                ansible_properties.set_property(property_name,
+                                                property_value)
 
         except Exception:
             raise Exception(traceback.format_exc())
@@ -88,47 +96,48 @@ class PropertyClear(Command):
         parser = super(PropertyClear, self).get_parser(prog_name)
         parser.add_argument('propertyname', metavar='<propertyname>',
                             help=u._('Property name'))
-        parser.add_argument('--hosts', nargs='?',
-                            metavar='<host_name>',
-                            help=u._('Property host'))
-        parser.add_argument('--groups', nargs='?',
-                            metavar='<group_name>',
-                            help=u._('Property group'))
+        parser.add_argument('--hosts', nargs=1,
+                            metavar='<host_list>',
+                            help=u._('Property host list'))
+        parser.add_argument('--groups', nargs=1,
+                            metavar='<group_list>',
+                            help=u._('Property group list'))
         return parser
 
     def take_action(self, parsed_args):
         try:
             property_name = parsed_args.propertyname.strip()
-            ansible_properties = properties.AnsibleProperties()
-            hosts = None
-            groups = None
 
             if parsed_args.hosts:
-                host_list = parsed_args.hosts.strip()
-                hosts = utils.convert_to_unicode(host_list).split(',')
+                if parsed_args.groups:
+                    raise CommandError(
+                        u._('Invalid to use both hosts and groups arguments ' +
+                            'together.'))
 
-            if parsed_args.groups:
-                group_list = parsed_args.groups.strip()
-                groups = utils.convert_to_unicode(group_list).split(',')
+                host_names = _get_names(parsed_args.hosts)
+                ansible_properties = \
+                    properties.AnsibleProperties(load_globals=False,
+                                                 load_groups=False,
+                                                 load_hosts=True)
+                ansible_properties.clear_host_property(property_name,
+                                                       host_names)
 
-            if hosts is None and groups is None:
+            elif parsed_args.groups:
+                group_names = _get_names(parsed_args.groups)
+                ansible_properties = \
+                    properties.AnsibleProperties(load_globals=False,
+                                                 load_groups=True,
+                                                 load_hosts=False)
+                ansible_properties.clear_group_property(property_name,
+                                                        group_names)
+
+            else:
+                ansible_properties = \
+                    properties.AnsibleProperties(load_globals=True,
+                                                 load_groups=False,
+                                                 load_hosts=False)
                 ansible_properties.clear_property(property_name)
-            elif hosts is not None and groups is not None:
-                raise CommandError(
-                    u._('Invalid to use both hosts and groups arguments '
-                        'together.'))
-            elif hosts is not None:
-                for host_name in hosts:
-                    if host_name == 'all':
-                        hosts = None
-                        break
-                ansible_properties.clear_host_property(property_name, hosts)
-            elif groups is not None:
-                for group_name in groups:
-                    if group_name == 'all':
-                        groups = None
-                        break
-                ansible_properties.clear_group_property(property_name, groups)
+
         except Exception:
             raise Exception(traceback.format_exc())
 
@@ -151,21 +160,18 @@ class PropertyList(Lister):
                             help=u._('List all properties'))
         parser.add_argument('--long', action='store_true',
                             help=u._('Show all property attributes'))
-        parser.add_argument('--hosts', nargs='?',
+        parser.add_argument('--hosts', nargs=1,
                             metavar='<host_list>',
                             help=u._('Property host list'))
-        parser.add_argument('--groups', nargs='?',
+        parser.add_argument('--groups', nargs=1,
                             metavar='<group_list>',
                             help=u._('Property group list'))
         return parser
 
     def take_action(self, parsed_args):
         try:
-            ansible_properties = properties.AnsibleProperties()
-
             if parsed_args.all:
                 self.is_all_flag = True
-
             if parsed_args.long:
                 self.is_long_flag = True
 
@@ -177,26 +183,32 @@ class PropertyList(Lister):
 
                 self.is_global = False
                 self.list_type = u._('Host')
-                host_list = parsed_args.hosts.strip()
-                host_names = \
-                    utils.convert_to_unicode(host_list).split(',')
-                if 'all' in host_names:
-                    host_names = None
+                host_names = _get_names(parsed_args.hosts)
+
+                ansible_properties = \
+                    properties.AnsibleProperties(load_globals=False,
+                                                 load_groups=False,
+                                                 load_hosts=True)
                 property_list = \
                     ansible_properties.get_host_list(host_names)
 
             elif parsed_args.groups:
                 self.is_global = False
                 self.list_type = u._('Group')
-                group_list = parsed_args.groups.strip()
-                group_names = \
-                    utils.convert_to_unicode(group_list).split(',')
-                if 'all' in group_names:
-                    group_names = None
+                group_names = _get_names(parsed_args.groups)
+                ansible_properties = \
+                    properties.AnsibleProperties(load_globals=False,
+                                                 load_groups=True,
+                                                 load_hosts=False)
                 property_list = \
                     ansible_properties.get_group_list(group_names)
 
             else:
+                ansible_properties = \
+                    properties.AnsibleProperties(load_globals=True,
+                                                 load_groups=False,
+                                                 load_hosts=False)
+
                 property_list = ansible_properties.get_all_unique()
 
             data = self._get_list_data(property_list)
