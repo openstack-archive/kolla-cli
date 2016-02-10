497988eb812cd803546a6ec53005d86a3d200da1
Borne Mace <borne.mace@oracle.com>
2016-02-10 09:20:20 -0800

    Added support to execute upgrade from cli.
    Jira-Issue: OPENSTACK-589

---
+++ kollacli/commands/upgrade.py
@@ -0,0 +1,38 @@
+# Copyright(c) 2016, Oracle and/or its affiliates.  All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+import logging
+import traceback
+
+import kollacli.i18n as u
+
+from kollacli.common.ansible.actions import upgrade
+
+from cliff.command import Command
+
+LOG = logging.getLogger(__name__)
+
+
+class Upgrade(Command):
+    """Upgrade"""
+    def get_parser(self, prog_name):
+        parser = super(Upgrade, self).get_parser(prog_name)
+        return parser
+
+    def take_action(self, parsed_args):
+        verbose_level = self.app.options.verbose_level
+        try:
+            upgrade(verbose_level)
+
+        except Exception:
+            raise Exception(traceback.format_exc())
