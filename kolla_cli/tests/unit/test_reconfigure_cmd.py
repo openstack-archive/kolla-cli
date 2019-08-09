acbbd130c6ed129b91305cd9c563aff63e2ee1b8
zhulingjie <easyzlj@gmail.com>
2019-08-09 23:31:08 +0800

    add reconfigure unit test
    Change-Id: I86ebb5e8a24927e43105fbfa72a405d31ab68e86

---
+++ kolla_cli/tests/unit/test_reconfigure_cmd.py
@@ -0,0 +1,30 @@
+# Copyright (c) 2018 OpenStack Foundation
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import mock
+
+from kolla_cli.tests.unit.common import KollaCliUnitTest
+
+
+class TestUnit(KollaCliUnitTest):
+    @mock.patch('kolla_cli.api.control_plane.ControlPlaneApi.reconfigure')
+    @mock.patch('kolla_cli.common.ansible.job.AnsibleJob.get_status')
+    @mock.patch('kolla_cli.shell.KollaCli._is_inventory_present',
+                return_value=True)
+    def test_reconfigure(self, _, mock_get_status, mock_reconfigure):
+        mock_get_status.return_value = 0
+        mock_reconfigure.return_value = self.get_fake_job()
+        ret = self.run_cli_command('action reconfigure')
+        self.assertEqual(ret, 0)
+        mock_reconfigure.assert_called_once_with(1, [], [])
