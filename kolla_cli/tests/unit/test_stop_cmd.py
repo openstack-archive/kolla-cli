b56875b1d2e4b459edac8ca90cc6b19300f39763
caoyuan <cao.yuan@99cloud.net>
2019-09-11 23:42:51 +0800

    Add stop unit test with services and hosts
    Change-Id: If8d47539c65d237941f6e64cf7770d7101ab960b

--- kolla_cli/tests/unit/test_stop_cmd.py
+++ kolla_cli/tests/unit/test_stop_cmd.py
@@ -40,3 +40,33 @@ class TestUnit(KollaCliUnitTest):
             hosts=','.join(hostnames)))
         self.assertEqual(ret, 0)
         mock_stop.assert_called_once_with(1, hostnames, [])
+
+    @mock.patch('kolla_cli.api.control_plane.ControlPlaneApi.stop')
+    @mock.patch('kolla_cli.common.ansible.job.AnsibleJob.get_status')
+    @mock.patch('kolla_cli.shell.KollaCli._is_inventory_present',
+                return_value=True)
+    def test_stop_with_services(self, _, mock_get_status, mock_stop):
+        mock_get_status.return_value = 0
+        mock_stop.return_value = self.get_fake_job()
+        services = ['service1', 'service2']
+        ret = self.run_cli_command(
+            'action stop --service {services}'.format(
+                services=','.join(services)))
+        self.assertEqual(ret, 0)
+        mock_stop.assert_called_once_with(1, [], services)
+
+    @mock.patch('kolla_cli.api.control_plane.ControlPlaneApi.stop')
+    @mock.patch('kolla_cli.common.ansible.job.AnsibleJob.get_status')
+    @mock.patch('kolla_cli.shell.KollaCli._is_inventory_present',
+                return_value=True)
+    def test_stop_with_hosts_and_services(self, _, mock_get_status,
+                                          mock_stop):
+        mock_get_status.return_value = 0
+        mock_stop.return_value = self.get_fake_job()
+        hostnames = ['host1', 'host2']
+        services = ['service1', 'service2']
+        ret = self.run_cli_command(
+            'action stop --hosts {hosts} --service {services}'.format(
+                hosts=','.join(hostnames), services=','.join(services)))
+        self.assertEqual(ret, 0)
+        mock_stop.assert_called_once_with(1, hostnames, services)
